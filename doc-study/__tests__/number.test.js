e0ea2220406f52a43eb853ac27f13250c7dd1284
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-01-12 09:21:23 -0300

    Number object study

--- doc-study/__tests__/number.test.js
+++ doc-study/__tests__/number.test.js
@@ -1,5 +1,13 @@
 const number = require('../number');
 
-test("Test Number.EPSILON property", () => {
-	expect(number.epsilon()).toBe(true);
+describe("Number Object", () => {
+	describe("Number properties", () => {
+		test("Test Number.EPSILON property", () => {
+			expect(number.epsilon()).toBe(true);
+		});
+		
+		test("Test MAX_SAFE_INTEGER", () => {
+			expect(number.safeInt()).toBe(9007199254740991);
+		});
+	});
 });
--- doc-study/number.js
+++ doc-study/number.js
@@ -1,4 +1,10 @@
+// Properties
 module.exports.epsilon = () => {
-	const result = Math.abs(0.2 - 0.3 + 0.1);
+	var result = Math.abs(0.2 - 0.3 + 0.1);
 	return result < Number.EPSILON;
+};
+
+module.exports.safeInt = () => {
+	var y = Number.MAX_SAFE_INTEGER;
+	return y;
 };
