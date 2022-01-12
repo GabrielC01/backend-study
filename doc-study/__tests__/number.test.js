4fb2a258080885c7570a456eaa4f17a2829a630a
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-01-12 11:12:26 -0300

    Number object study

--- doc-study/__tests__/number.test.js
+++ doc-study/__tests__/number.test.js
@@ -2,12 +2,72 @@ const number = require('../number');
 
 describe("Number Object", () => {
 	describe("Number properties", () => {
-		test("Test Number.EPSILON property", () => {
-			expect(number.epsilon()).toBe(true);
+
+		describe("EPSILON", () => {
+			test("Return EPSILON.", () => {
+				expect(number.returnEpsilon()).toBe(2**-52);
+			});
+
+			test("Test EPSILON property.", () => {
+				expect(number.epsilonComparison()).toBe(true);
+			});
 		});
 		
-		test("Test MAX_SAFE_INTEGER", () => {
-			expect(number.safeInt()).toBe(9007199254740991);
+		describe("MAX_SAFE_INTEGER", () => {
+			test("Return MAX_SAFE_INTEGER value.", () => {
+				expect(number.returnMaxSafeInt()).toBe(9007199254740991);
+			});
+
+			test("Try isSafeInteger() wtesth MAX_SAFE_INTEGER.", () => {
+				expect(number.isSafe(0)).toBe(true);
+			});
+
+			test("Try isSafeInteger() wtesth MAX_SAFE_INTEGER + 1.", () => {
+				expect(number.isSafe(1)).toBe(false);
+			});
+		});
+
+		describe("MAX_VALUE", () => {
+			test("Return MAX_VALUE", () => {
+				expect(number.returnMaxValue(0)).toBe(1.7976931348623157e+308);
+			});
+
+			test("Add 1 to MAX_VALUE", () => {
+				expect(number.returnMaxValue(10)).toBe("Process as Infinity");
+			});
+		});
+
+		describe("MIN_SAFE_INTEGER", () => {
+			test("Return MIN_SAFE_INTEGER", () => {
+				expect(number.returnMinSafeInt()).toBe(-9007199254740991);
+			});
+		});
+
+		describe("MIN_VALUE", () => {
+			test("Return MIN_VALUE", () => {
+				expect(number.returnMinValue()).toBe(5e-324);
+			});
+		});
+
+		describe("NaN", () => {
+			test("Return NaN", () => {
+				expect(number.returnNaN()).toBe(NaN);
+			});
+		});
+
+		describe("NEGATIVE_INFINITY", () => {
+			test("Return NEGATIVE_INFINITY", () => {
+				expect(number.returnNegativeInfinity()).toBe(-Infinity);
+			});
+		});
+
+		describe("POSITIVE_INFINITY", () => {
+			test("Return POSITIVE_INFINITY", () => {
+				expect(number.returnPositiveInfinity()).toBe(Infinity);
+			});
+		});
+
+		describe("", () => {
 		});
 	});
 });
--- doc-study/number.js
+++ doc-study/number.js
@@ -1,10 +1,46 @@
 // Properties
-module.exports.epsilon = () => {
+module.exports.returnEpsilon = () => {
+	return Number.EPSILON;
+};
+
+module.exports.epsilonComparison = () => {
 	var result = Math.abs(0.2 - 0.3 + 0.1);
 	return result < Number.EPSILON;
 };
 
-module.exports.safeInt = () => {
+module.exports.returnMaxSafeInt = () => {
 	var y = Number.MAX_SAFE_INTEGER;
 	return y;
+};
+
+module.exports.isSafe = (x) => {
+	var y = Number.MAX_SAFE_INTEGER + x;
+	return Number.isSafeInteger(y);
+};
+
+module.exports.returnMaxValue = (x) => {
+	if (x > 0 && Number.MAX_VALUE + x === Number.MAX_VALUE)
+		return "Process as Infinity";
+	else
+		return Number.MAX_VALUE + x;
+};
+
+module.exports.returnMinSafeInt = () => {
+	return Number.MIN_SAFE_INTEGER;
+};
+
+module.exports.returnMinValue = () => {
+	return Number.MIN_VALUE;
+};
+
+module.exports.returnNaN = () => {
+	return Number.NaN;
+};
+
+module.exports.returnNegativeInfinity = () => {
+	return Number.NEGATIVE_INFINITY;
+};
+
+module.exports.returnPositiveInfinity = () => {
+	return Number.POSITIVE_INFINITY;
 };
