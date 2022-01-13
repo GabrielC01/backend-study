61e1722b387643676f1b5f1b1b60bd13aa05beba
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-01-13 18:30:50 -0300

    Simplified testing

---
+++ doc-study/number.test.js
@@ -0,0 +1,123 @@
+describe("Number Object", () => {
+	describe("Number properties", () => {
+		describe("EPSILON", () => {
+			test("Return EPSILON.", () => {
+				expect(Number.EPSILON).toBe(2**-52);
+			});
+		});
+		
+		describe("MAX_SAFE_INTEGER", () => {
+			test("Return MAX_SAFE_INTEGER value.", () => {
+				expect(Number.MAX_SAFE_INTEGER).toBe(9007199254740991);
+			});
+		});
+
+		describe("MAX_VALUE", () => {
+			test("Return MAX_VALUE", () => {
+				expect(Number.MAX_VALUE).toBe(1.7976931348623157e+308);
+			});
+
+			test("Add 1 to MAX_VALUE", () => {
+				expect(Number.MAX_VALUE).toBe(1.7976931348623157e+308);
+			});
+		});
+
+		describe("MIN_SAFE_INTEGER", () => {
+			test("Return MIN_SAFE_INTEGER", () => {
+				expect(Number.MIN_SAFE_INTEGER).toBe(-9007199254740991);
+			});
+		});
+
+		describe("MIN_VALUE", () => {
+			test("Return MIN_VALUE", () => {
+				expect(Number.MIN_SAFE_INTEGER).toBe(-9007199254740991);
+			});
+		});
+
+		describe("NaN", () => {
+			test("Return NaN", () => {
+				expect(Number.NaN).toBe(NaN);
+			});
+		});
+
+		describe("NEGATIVE_INFINITY", () => {
+			test("Return NEGATIVE_INFINITY", () => {
+				expect(Number.NEGATIVE_INFINITY).toBe(-Infinity);
+			});
+		});
+
+		describe("POSITIVE_INFINITY", () => {
+			test("Return POSITIVE_INFINITY", () => {
+				expect(Number.POSITIVE_INFINITY).toBe(Infinity);
+			});
+		});
+	});
+
+	describe("Number Methods", () => {
+		describe("isFinite()", () => {
+			test("arg: Infinity", () => {
+				expect(Number.isFinite(Infinity)).toBe(false);
+			});
+
+			test("arg: NaN", () => {
+				expect(Number.isFinite(NaN)).toBe(false);
+			});
+
+			test("arg: -Infinity", () => {
+				expect(Number.isFinite(-Infinity)).toBe(false);
+			});
+
+			test("arg: 0", () => {
+				expect(Number.isFinite(0)).toBe(true);
+			});
+
+			test("arg: 2e64", () => {
+				expect(Number.isFinite(2e64)).toBe(true);
+			});
+		});
+
+		describe("isInteger()", () => {
+			test("arg: 0", () => {
+				expect(Number.isInteger(0)).toBe(true);
+			});
+
+			test("arg: 1", () => {
+				expect(Number.isInteger(1)).toBe(true);
+			});
+
+			test("arg: -100000", () => {
+				expect(Number.isInteger(-100000)).toBe(true);
+			});
+
+			test("arg: Infinity", () => {
+				expect(Number.isInteger(Infinity)).toBe(false);
+			});
+		});
+
+		describe("isNaN", () => {
+			test("arg: NaN", () => {
+				expect(Number.isNaN(NaN)).toBe(true);
+			});
+
+			test("arg: 37", () => {
+				expect(Number.isNaN(37)).toBe(false);
+			});
+
+			test("arg: 0 / 0", () => {
+				expect(Number.isNaN(0 / 0)).toBe(true);
+			});
+		});
+
+		describe("isSafeInteger", () => {
+			test("arg: MAX_SAFE_INTEGER", () => {
+				expect(Number.isSafeInteger(Number.MAX_SAFE_INTEGER)).toBe(true);
+			});
+
+			test("Try isSafeInteger() with MAX_SAFE_INTEGER + 1.", () => {
+				expect(Number.isSafeInteger(Number.MAX_SAFE_INTEGER + 1)).toBe(false);
+			});
+		});
+//		describe("", () => {
+//		});
+	});
+});
