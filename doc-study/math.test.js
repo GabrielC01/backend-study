2f45fdcdb4ec017f2346563bfac807cc4867d3d2
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-01-14 11:14:22 -0300

    Math.E

---
+++ doc-study/math.test.js
@@ -0,0 +1,22 @@
+describe("Math Object", () => {
+	describe("Properties", () => {
+
+		// Euler's constant. Approximately 2.718.
+		describe("E", () => {
+			test("Check Euler's constant", () => {
+				expect(Math.E).toBeCloseTo(2.718);
+			});
+		});
+
+//		describe("", () => {
+//		});
+//		describe("", () => {
+//		});
+//		describe("", () => {
+//		});
+//		describe("", () => {
+//		});
+//		describe("", () => {
+//		});
+	});
+});
