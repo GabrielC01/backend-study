5cc5b576ccbc931fc7812a0f3f48080a44809252
GabrielC01 <gabcchaves@zohomail.com>
2022-01-25 23:29:17 -0300

    Get current date

--- doc-study/dateExercises.test.js
+++ doc-study/dateExercises.test.js
@@ -1,3 +1,10 @@
+// Utilities
+function datePad(number) {
+	if (number < 10)
+		return  '0' + number;
+}
+
+// Exercises
 test('Check if input is a date object', () => {
 	function isDate(input) {
 		return input instanceof Date;
@@ -6,4 +13,12 @@ test('Check if input is a date object', () => {
 	expect(isDate(new Date(86400000))).toBeTruthy();
 	expect(isDate(new Date(99, 5, 24, 11, 33, 30, 0))).toBeTruthy();
 	expect(isDate([1, 2, 4, 0])).toBeFalsy();
+});
+
+test('Return the current date', () => {
+	function curday(sep) {
+		let cur = new Date();
+		return '' + (cur.getMonth() + 1) + sep + cur.getDate() + sep + cur.getYear();
+	}
+	expect(/\d+\/\d+\/\d+/.test(curday('/'))).toBeTruthy();
 });
