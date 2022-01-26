2a92a983c56ee97d96349970407b3986bda37743
GabrielC01 <gabcchaves@zohomail.com>
2022-01-26 10:39:25 -0300

    Return the number of days in the given month

--- doc-study/dateExercises.test.js
+++ doc-study/dateExercises.test.js
@@ -22,3 +22,19 @@ test('Return the current date', () => {
 	}
 	expect(/\d+\/\d+\/\d+/.test(curday('/'))).toBeTruthy();
 });
+
+test('Return the number of days in a month', () => {
+	function getDaysInMonth(monthIndex, year) {
+		let date = new Date(year, monthIndex-1);
+		date.setDate(30);
+		if (date.getMonth() > monthIndex-1)
+			return year % 4 == 0 ? 29 : 28;
+		date.setDate(31);
+		return date.getMonth() > monthIndex-1 ? 30 : 31;
+	}
+
+	expect(getDaysInMonth(1, 2012)).toBe(31);
+	expect(getDaysInMonth(2, 2012)).toBe(29);
+	expect(getDaysInMonth(9, 2012)).toBe(30);
+	expect(getDaysInMonth(12, 2012)).toBe(31);
+});
