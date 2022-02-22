63d0459d07676382c928160a0df0428ec3b9cce6
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-02-22 19:35:46 -0300

    Complete a Promise with resolve and reject

--- es6/promises/promise.js
+++ es6/promises/promise.js
@@ -1,7 +1,12 @@
 const myPromise = new Promise((resolve, reject) => {
-  
+   
 });
 
 const makeServerRequest = new Promise((resolve, reject) => {
-
+  let responseFromServer;
+  if (responseFromServer) {
+    resolve('We got the data');
+  } else {
+    reject('Data not received');
+  }
 });
