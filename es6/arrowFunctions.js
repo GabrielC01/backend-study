4ad91c01caf0fb55cd73cc5be5cb418e843d7d75
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-02-22 10:22:13 -0300

    Write Arrow Functions with Parameters

--- es6/arrowFunctions.js
+++ es6/arrowFunctions.js
@@ -7,3 +7,8 @@ const myNewFunc = () => "value";
 
 console.log(myFunc());
 console.log(myNewFunc());
+
+const pow = (base, power) => base**power;
+console.log(pow(2, 2));
+const doubler = item => item * 2; 
+console.log(doubler(2));
