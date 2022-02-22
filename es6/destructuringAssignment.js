8776de116a6e62f05e016d9b3cae2535cfa5b406
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-02-22 11:17:20 -0300

    Use Destructuring Assignment to Assign Variables from Arrays

--- es6/destructuringAssignment.js
+++ es6/destructuringAssignment.js
@@ -35,3 +35,13 @@ const { johnDoe: { age, email }} = user;
 const { johnDoe: { age: userAge, email: userEmail }} = user;
 console.log(age);
 console.log(userAge);
+
+let [a, b] = [1, 2, 3, 4, 5, 6, 7];
+console.log(a, b);
+let [x, y,,, z] = [1, 2, 3, 4, 5, 6, 7];
+console.log(x, y, z);
+
+a = 8;
+b = 6;
+[a, b] = [b, a];
+console.log(a, b);
