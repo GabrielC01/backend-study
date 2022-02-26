c33ecbff1b9a021b37dd4e8118358619ce52ac2d
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-02-26 15:21:00 -0300

    update

--- ajax/get_started/makeRequest.js
+++
@@ -1,7 +0,0 @@
-// Create an instance of XMLHttpRequest
-var httpRequest = new XMLHttpRequest();
-
-// Reference the function to be called when state changes 
-httpRequest.onreadystatechange = () => {
-  // Process the response from the server
-}
